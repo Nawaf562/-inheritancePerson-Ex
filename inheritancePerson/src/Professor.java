089ec1ad1e3987695197bb434af5f4d6696bea48
TOSHIBA <TOSHIBA@DESKTOP-SEP3065>
2021-03-23 02:17:43 +0300

    The second lift

--- inheritancePerson/src/Professor.java
+++ inheritancePerson/src/Professor.java
@@ -8,14 +8,12 @@ public class Professor extends Person{
 	info.PhoneNumber = "050436";
 	info.EmailAddress = "300400@nu.edu.sa";
 			
-			double salary = 10000;
+			double Salary = 23458;
 			 System.out.println("Name is " + info.Name);
 		     System.out.println("PhoneNumber is " + info.PhoneNumber);
 		     System.out.println("Email is " + info.EmailAddress);
-		     System.out.println("Salary is " + salary);
+		     System.out.println("Salary is " + Salary);
 	}
 			
-	int Salary = 23458;
-	
 
 }
